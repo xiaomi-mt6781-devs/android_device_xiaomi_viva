f726025f0a9baee5a9c07c2f4c3677c8540afc8a
bengris32 <bengris32@protonmail.ch>
2022-08-17 21:34:49 +0100

    viva: Link mtk_agpsd against V32 libcrypto
    * cannot locate symbol "lh_free" referenced by "/vendor/bin/mtk_agpsd"...
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -220,10 +220,11 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1.vendor:64 \
     android.hardware.graphics.composer@2.1-resources.vendor:64
 
-# VNDK 32 libsutils (required by PQ)
+# VNDK 32 libs
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm/arch-arm-armv7-a-neon/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so \
+	prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v32.so
 
 # DRM
 PRODUCT_PACKAGES += \
