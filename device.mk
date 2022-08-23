c252502a25667609c90099dd4c0374daf6639e35
Josh Wu <joshwu@google.com>
2022-08-23 11:36:11 +0100

    viva: Switch android.hardware.bluetooth.audio to AIDL
    Test: m
    Bug: 203490261

--- device.mk
+++ device.mk
@@ -187,7 +187,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.1-impl
+	android.hardware.bluetooth.audio-impl
 
 # Camera
 PRODUCT_PACKAGES += \
