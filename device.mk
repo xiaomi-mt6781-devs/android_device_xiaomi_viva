89860208dc1b2fc1ba02991b82a54a5b7dd81756
Woomy <woomy@woomy.be>
2022-10-10 19:25:55 +0200

    viva/blobs: Build gatekeeper impl/service from source
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -76,6 +76,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl
+
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH)
 
