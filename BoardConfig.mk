a3bdcbe1a7b0df96bd383cb55d2ea6aa0e97c54d
Woomy <woomy@woomy.be>
2022-10-22 12:23:33 +0200

    viva/props: Add initial props
    * They will be sorted in the future
    
    Signed-off-by: Woomymy <woomy@woomy.be>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,8 @@ KERNEL_PATH := device/xiaomi/viva-kernel
 # Broken build rules
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # Architecture flags
 TARGET_ARCH := arm64
