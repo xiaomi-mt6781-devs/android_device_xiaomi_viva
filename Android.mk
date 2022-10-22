a6e0c0adf4f149b0e3643334606eec4c097bc766
Woomy <woomy@woomy.be>
2022-10-22 12:27:54 +0200

    viva/Android.mk: Handle vendor symlinks properly
    * vulkan.${ro.hardware}.so libs need to be symlinked to
      /vendor/lib(64)/egl/libGLES_mali.so
    
    * gatekeeper default implementations are symlinked to
      libSoftGatekeeper.so
    
    * Some vendor libs like libdpframework, libpq_propt, libmtk_drvb and
      libnir_neon_driver need to be symlinked to mt6781/${lib}.so, also
      symlink them properly
    
    Signed-off-by: Woomymy <woomy@woomy.be>

--- Android.mk
+++ Android.mk
@@ -6,4 +6,37 @@
 
 ifeq ($(TARGET_DEVICE),viva)
 include $(call all-makefiles-under,$(call my-dir))
+include $(CLEAR_VARS)
+
+VENDOR_LIB_LINKS := \
+	$(TARGET_OUT_VENDOR)/lib/libdpframework.so \
+	$(TARGET_OUT_VENDOR)/lib64/libdpframework.so \
+	$(TARGET_OUT_VENDOR)/lib/libmtk_drvb.so \
+	$(TARGET_OUT_VENDOR)/lib64/libmtk_drvb.so \
+	$(TARGET_OUT_VENDOR)/lib/libnir_neon_driver.so \
+	$(TARGET_OUT_VENDOR)/lib64/libnir_neon_driver.so \
+	$(TARGET_OUT_VENDOR)/lib/libpq_prot.so \
+	$(TARGET_OUT_VENDOR)/lib64/libpq_prot.so
+
+GATEKEEPER_SYMLINKS := \
+	$(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so \
+	$(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+
+VULKAN_SYMLINKS := \
+	$(TARGET_OUT_VENDOR)/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+
+$(VENDOR_LIB_LINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Linking $(notdir $@)"
+	@ln -svf $(TARGET_BOARD_PLATFORM)/$(notdir $@) $@
+
+$(GATEKEEPER_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Linking $@"
+	@ln -svf libSoftGatekeeper.so $@
+
+$(VULKAN_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@ln -svf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+	@ln -svf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_LIB_LINKS) $(GATEKEEPER_SYMLINKS) $(VULKAN_SYMLINKS)
+
 endif
