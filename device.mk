51c6a232251925dd9bc78d619b3c0f4be81c2610
Woomy <woomy@woomy.be>
2022-10-22 13:40:15 +0200

    viva/device: Build dumpstates interfaces from source
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -200,6 +200,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.cas@1.2-service-lazy
 
+# Dumpstate
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.0.vendor \
+    android.hardware.dumpstate@1.1.vendor
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
