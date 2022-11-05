0ca1ca92d1706916d98d357f08bd9bb9b2201147
Woomy <woomy@woomy.be>
2022-11-05 11:54:16 +0100

    viva/device: Switch to AIDL MultiHAL sensor service
    * Introduced in Android T
    
    Test: Boots, `getprop init.svc.vendor.sensors-hal-multihal == running`, `dumpsys sensorservice`, sensors works
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -345,7 +345,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors-service.multihal \
     libsensorndkbridge
 
 # Sensors MultiHAL config
