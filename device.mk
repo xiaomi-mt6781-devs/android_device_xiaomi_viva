f056020cac1de72be961cb72153d4f11b9296d4c
Woomy <woomy@woomy.be>
2022-11-05 17:20:05 +0100

    viva/device: Build android.hardware.drm@1.0-4
    * Required by widevine
    
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -227,6 +227,11 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+	android.hardware.drm@1.0.vendor \
+	android.hardware.drm@1.1.vendor \
+	android.hardware.drm@1.2.vendor \
+	android.hardware.drm@1.3.vendor \
+	android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
 # Fingerprint
