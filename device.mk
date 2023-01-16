63f22da23f1c0eb4cf442a3ad28c67aa2701e5f1
Woomymy <woomy@woomy.be>
2023-01-16 19:11:06 +0100

    viva/device: Build android.hardware.themral@1.0-impl
    Signed-off-by: Woomymy <woomy@woomy.be>

--- device.mk
+++ device.mk
@@ -376,7 +376,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0.vendor \
+	android.hardware.themral@1.0-impl \
     android.hardware.thermal@2.0.vendor
 
 # USB
