5f8f44f5f9b5ca86021ffc3639ba52b19ee12e31
Woomymy <woomy@woomy.be>
2023-01-21 18:05:44 +0100

    viva/makefiles: Arrow -> Lineage
    Signed-off-by: Woomymy <woomy@woomy.be>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,12 @@
 #
 # Copyright (C) 2022 ArrowOS
+# Copyright (C) 2023 LineageOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/arrow_viva.mk
+	$(LOCAL_DIR)/lineage_viva.mk
 
 COMMON_LUNCH_CHOICES := \
-	$(foreach FLAVOR, user userdebug eng, arrow_viva-$(FLAVOR))
+	$(foreach FLAVOR, user userdebug eng, lineage_viva-$(FLAVOR))
--- lineage_viva.mk
+++ lineage_viva.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2022 ArrowOS
+# Copyright (C) 2023 LineageOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common ArrowOS Stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common LineageOS Stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_viva
+PRODUCT_NAME := lineage_viva
 PRODUCT_DEVICE := viva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
