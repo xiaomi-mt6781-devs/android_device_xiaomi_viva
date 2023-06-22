ede4b670fd7936c3b085ba66b480bd873647e1fc
Woomymy <woomy@woomy.be>
2023-06-22 20:22:17 +0200

    viva: vintf: Add lineage device_framework_matrix to DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE
    Signed-off-by: Woomymy <woomy@woomy.be>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,7 +122,9 @@ OVERRIDE_TARGET_FLATTEN_APEX := true
 # VINTF
 DEVICE_MANIFEST_FILE := $(CONFIGS_PATH)/vintf/manifest.xml
 PRODUCT_COMPATIBILITY_MATRIX_FILE := $(CONFIGS_PATH)/vintf/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(CONFIGS_PATH)/vintf/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(CONFIGS_PATH)/vintf/framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Properties
 TARGET_SYSTEM_PROP += $(CONFIGS_PATH)/props/system.prop
